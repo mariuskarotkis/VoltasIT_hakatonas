6157d8af60b8b1d13e71756c5625f795198db753
marius.karotkis <marius.karotkis@gmail.com>
2021-09-18 12:39:07 +0300

    Add bluetooth connection class

--- app/src/main/java/com/voltasit/voltasit_hakatonas/MainActivity.java
+++ app/src/main/java/com/voltasit/voltasit_hakatonas/MainActivity.java
@@ -1,6 +1,13 @@
 package com.voltasit.voltasit_hakatonas;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -12,9 +19,45 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.voltasit.voltasit_hakatonas.databinding.ActivityMainBinding;
 
+import java.util.UUID;
+
+
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+
+
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
+    public final static String MODULE_MAC = "98:D3:34:90:6F:A1";
+    public final static int REQUEST_ENABLE_BT = 1;
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+
+    BluetoothAdapter bta;
+    BluetoothSocket mmSocket;
+    BluetoothDevice mmDevice;
+    ConnectedThread btt = null;
+    Button btnGetData, btnSendData;
+    TextView response;
+//    boolean relayFlag = true;
+    public Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +75,122 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        Log.i("[BLUETOOTH]", "Creating listeners");
+        response = (TextView) findViewById(R.id.response);
+        btnGetData = (Button) findViewById(R.id.btnGetData);
+        btnSendData = (Button) findViewById(R.id.btnSendData);
+
+
+
+        btnGetData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("[BLUETOOTH]", "Attempting to send data");
+                if (mmSocket.isConnected() && btt != null) { //if we have connection to the bluetoothmodule
+//                    if(relayFlag){
+//                        String sendtxt = "RY";
+//                        btt.write(sendtxt.getBytes());
+//                        relayFlag = false;
+//                    }else{
+//                        String sendtxt = "RN";
+//                        btt.write(sendtxt.getBytes());
+//                        relayFlag = true;
+//                    }
+
+                    //disable the button and wait for 4 seconds to enable it again
+                    btnGetData.setEnabled(false);
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try{
+                                Thread.sleep(4000);
+                            }catch(InterruptedException e){
+                                return;
+                            }
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    btnGetData.setEnabled(true);
+                                }
+                            });
+
+                        }
+                    }).start();
+                } else {
+                    Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+
+
+
+        bta = BluetoothAdapter.getDefaultAdapter();
+
+        //if bluetooth is not enabled then create Intent for user to turn it on
+        if(!bta.isEnabled()){
+            Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBTIntent, REQUEST_ENABLE_BT);
+        }else{
+            initiateBluetoothProcess();
+        }
+
+    }
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK && requestCode == REQUEST_ENABLE_BT){
+            initiateBluetoothProcess();
+        }
+    }
+
+    public void initiateBluetoothProcess(){
+
+        if(bta.isEnabled()){
+
+            //attempt to connect to bluetooth module
+            BluetoothSocket tmp = null;
+            mmDevice = bta.getRemoteDevice(MODULE_MAC);
+
+            //create socket
+            try {
+                tmp = mmDevice.createRfcommSocketToServiceRecord(MY_UUID);
+                mmSocket = tmp;
+                mmSocket.connect();
+                Log.i("[BLUETOOTH]","Connected to: "+mmDevice.getName());
+            }catch(IOException e){
+                try{mmSocket.close();}catch(IOException c){return;}
+            }
+
+            Log.i("[BLUETOOTH]", "Creating handler");
+            mHandler = new Handler(Looper.getMainLooper()){
+                @Override
+                public void handleMessage(Message msg) {
+                    //super.handleMessage(msg);
+                    if(msg.what == ConnectedThread.RESPONSE_MESSAGE){
+                        String txt = (String)msg.obj;
+                        if(response.getText().toString().length() >= 30){
+                            response.setText("");
+                            response.append(txt);
+                        }else{
+                            response.append("\n" + txt);
+                        }
+                    }
+                }
+            };
+
+            Log.i("[BLUETOOTH]", "Creating and running Thread");
+            btt = new ConnectedThread(mmSocket,mHandler);
+            btt.start();
+        }
     }
 
 }
